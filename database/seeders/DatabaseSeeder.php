0eeeae0ffde5a59c79c0ae609d7040e66f655a53
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-04 14:08:57 +0700

    feat: Set up composer.lock, add a image & make some seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace Database\Seeders;
-use App\Models\User;
-use App\Models\Leader;
+
 use App\Models\Email;
-use Illuminate\Support\Facades\Hash;
+use App\Models\Leader;
+use App\Models\User;
+use Illuminate\Database\Seeder;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -26,9 +27,34 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         User::create([
-            'email' => 'test@example.com',
-            'password' => Hash::make('password'),
-            'role' => 'Admin'
+            'email' => 'adminuser@example.com',
+            'password' => Hash::make('useradmin123'),
+            'role' => 'Admin',
+        ]);
+
+        User::create([
+            'email' => 'useruser@example.com',
+            'password' => Hash::make('useruser123'),
+        ]);
+
+        Leader::create([
+            'name' => 'Alip',
+            'rationalization' => 'Rationalization 1',
+            'photo' => '/public/image/Alip.jpeg',
+        ]);
+
+        Leader::create([
+            'name' => 'Farhan',
+            'rationalization' => 'Rationalization 2',
+            'photo' => '/public/image/Farhan.jpeg',
+        ]);
+
+        Email::create([
+            'email' => 'adminuser@example.com',
+        ]);
+
+        Email::create([
+            'email' => 'useruser@example.com',
         ]);
     }
 }
